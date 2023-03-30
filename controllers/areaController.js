f141a1425dc06d56b0f4ebf9fa2548aa32d30e9b
fairplay012 <84314350+fairplay012@users.noreply.github.com>
2023-03-30 15:10:33 +0700

    upđate

--- controllers/areaController.js
+++ controllers/areaController.js
@@ -6,11 +6,14 @@ const areaController = {
         try {
             const newArea  = new Area(req.body);
             const savedArea = await newArea.save();
-            res.status(200).json(savedArea);
-        } catch (error) {
+
+                res.status(200).json(savedArea);
+            // console.log(newArea)
+        } catch (error){
             res.status(500).json(error);
         }
     },
 }
+
 
 module.exports = areaController;
